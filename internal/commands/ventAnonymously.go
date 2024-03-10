475686f45358e832d127c07622daeb505de6a9f2
Nick Lesseos <lesseosnick@gmail.com>
2024-03-10 12:08:13 -0700

    Added: reply anonymously

--- internal/commands/ventAnonymously.go
+++ internal/commands/ventAnonymously.go
@@ -1,100 +1,94 @@
 package commands
 
 import (
-	"fmt"
 	"log"
-	"time"
 
-	"github.com/bromigos-org/bromigo/internal/utils"
 	"github.com/bwmarrin/discordgo"
 )
 
-const ventChannelName = "vent-anonymously" // Replace with your actual channel name
+const ventChannelName = "vent-anonymously"
+
+// Use a map to track the IDs of threads recreated by the bot for anonymity.
+var anonymousThreads = make(map[string]bool)
+
 func VentAnonymously(s *discordgo.Session, m *discordgo.MessageCreate) {
 	// Ignore messages created by the bot itself
 	if m.Author.ID == s.State.User.ID {
 		return
 	}
 
-	// Find the channel (or thread) where the message was posted
 	channel, err := s.Channel(m.ChannelID)
 	if err != nil {
 		log.Printf("Error retrieving channel: %v\n", err)
 		return
 	}
 
-	utils.LogJSON("Channel", channel)
-
-	utils.LogJSON("Message", m)
-
 	// Check if the message was posted in a thread under the vent-anonymously channel
-	isThread := channel.Type == discordgo.ChannelTypeGuildPublicThread || channel.Type == discordgo.ChannelTypeGuildPrivateThread
-	if isThread {
+	if isThreadUnderVentChannel(s, channel) {
 		parentChannel, err := s.Channel(channel.ParentID)
 		if err != nil {
 			log.Printf("Error retrieving parent channel: %v\n", err)
 			return
 		}
 
-		if parentChannel.Name == ventChannelName {
-
-			fmt.Println(channel.Name)
+		if _, managed := anonymousThreads[channel.ID]; !managed {
+			// Delete the original thread
+			_, err = s.ChannelDelete(channel.ID)
+			if err != nil {
+				log.Printf("Error deleting original thread: %v\n", err)
+				return
+			}
 
 			threadStart := discordgo.ThreadStart{
 				Name: channel.Name,
 			}
 
-			mess, err := s.ChannelMessage(channel.ID, channel.LastMessageID)
-			if err != nil {
-				log.Printf("Error retrieving message: %v\n", err)
-				return
-			}
-
-			fmt.Printf("Message: %+v\n", mess)
-
-			// // Get first message
-			// messages, err := s.ChannelMessages(channel.ID, 1, "", "", "")
-			// if err != nil {
-			// 	log.Printf("Error retrieving messages: %v\n", err)
-			// 	return
-			// }
-
-			// // Check if there are any messages to avoid index out-of-range errors
-			// if len(messages) > 0 {
-			// 	// Print the first message content. No need to use & as we're accessing a field of the struct pointed to by the slice element.
-			// 	fmt.Printf("First Message Content: %s\n", messages[0].Content)
-
-			// 	// If you want to print the entire message struct, use %+v to print with field names for readability.
-			// 	fmt.Printf("First Message Struct: %+v\n", *messages[0])
-			// } else {
-			// 	fmt.Println("No messages found in the thread.")
-			// }
-
-			// Define thread creation parameter bs
+			// Define thread creation parameters for the new anonymous thread
 			threadCreateData := discordgo.MessageSend{
-				// Content: m.Content,
-				Content: "This thread was created to replace a deleted thread.",
+				Content: m.Content,
 			}
 
-			// // Delete the thread
-			// _, err = s.ChannelDelete(channel.ID)
-			// if err != nil {
-			// 	log.Printf("Error deleting thread: %v\n", err)
-			// 	return
-			// }
-
-			// Create a new thread with the same name as the deleted thread
+			// Recreate the thread anonymously
 			newThread, err := s.ForumThreadStartComplex(parentChannel.ID, &threadStart, &threadCreateData)
 			if err != nil {
-				log.Printf("Error creating new thread: %v\n", err)
+				log.Printf("Error creating new anonymous thread: %v\n", err)
 				return
 			}
-			time.Sleep(2 * time.Second)
-			// Post the message content as the bot in the new thread
-			_, err = s.ChannelMessageSend(newThread.ID, m.ContentWithMentionsReplaced())
-			if err != nil {
-				log.Printf("Error sending message in new thread: %v\n", err)
-			}
+
+			// Mark the new thread as managed
+			anonymousThreads[newThread.ID] = true
 		}
 	}
+}
+
+func HandleThreadMessages(s *discordgo.Session, m *discordgo.MessageCreate) {
+	// Ignore messages created by the bot itself or messages not in anonymous threads
+	if m.Author.ID == s.State.User.ID || !anonymousThreads[m.ChannelID] {
+		return
+	}
+
+	// Delete the original message
+	err := s.ChannelMessageDelete(m.ChannelID, m.ID)
+	if err != nil {
+		log.Printf("Error deleting message: %v\n", err)
+		return
+	}
+
+	// Repost the message content as the bot in the same thread
+	_, err = s.ChannelMessageSend(m.ChannelID, "Anonymous: "+m.Content)
+	if err != nil {
+		log.Printf("Error reposting message as bot: %v\n", err)
+	}
+}
+
+func isThreadUnderVentChannel(s *discordgo.Session, channel *discordgo.Channel) bool {
+	if channel.Type != discordgo.ChannelTypeGuildPublicThread && channel.Type != discordgo.ChannelTypeGuildPrivateThread {
+		return false
+	}
+	parentChannel, err := s.Channel(channel.ParentID)
+	if err != nil {
+		log.Printf("Error retrieving parent channel: %v\n", err)
+		return false
+	}
+	return parentChannel.Name == ventChannelName
 }
