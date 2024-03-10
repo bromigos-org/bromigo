262b8c5238a73b9ea61233b4a958d8462ca761d0
Nick Lesseos <lesseosnick@gmail.com>
2024-03-09 18:23:59 -0800

    Added: get message from channel and utils

--- internal/commands/ventAnonymously.go
+++ internal/commands/ventAnonymously.go
@@ -1,9 +1,11 @@
 package commands
 
 import (
+	"fmt"
 	"log"
 	"time"
 
+	"github.com/bromigos-org/bromigo/internal/utils"
 	"github.com/bwmarrin/discordgo"
 )
 
@@ -21,6 +23,10 @@ func VentAnonymously(s *discordgo.Session, m *discordgo.MessageCreate) {
 		return
 	}
 
+	utils.LogJSON("Channel", channel)
+
+	utils.LogJSON("Message", m)
+
 	// Check if the message was posted in a thread under the vent-anonymously channel
 	isThread := channel.Type == discordgo.ChannelTypeGuildPublicThread || channel.Type == discordgo.ChannelTypeGuildPrivateThread
 	if isThread {
@@ -31,22 +37,51 @@ func VentAnonymously(s *discordgo.Session, m *discordgo.MessageCreate) {
 		}
 
 		if parentChannel.Name == ventChannelName {
-			// Delete the thread
-			_, err := s.ChannelDelete(m.ChannelID)
-			if err != nil {
-				log.Printf("Error deleting thread: %v\n", err)
-				return
-			}
+
+			fmt.Println(channel.Name)
 
 			threadStart := discordgo.ThreadStart{
 				Name: channel.Name,
 			}
 
-			// Define thread creation parameters
+			mess, err := s.ChannelMessage(channel.ID, channel.LastMessageID)
+			if err != nil {
+				log.Printf("Error retrieving message: %v\n", err)
+				return
+			}
+
+			fmt.Printf("Message: %+v\n", mess)
+
+			// // Get first message
+			// messages, err := s.ChannelMessages(channel.ID, 1, "", "", "")
+			// if err != nil {
+			// 	log.Printf("Error retrieving messages: %v\n", err)
+			// 	return
+			// }
+
+			// // Check if there are any messages to avoid index out-of-range errors
+			// if len(messages) > 0 {
+			// 	// Print the first message content. No need to use & as we're accessing a field of the struct pointed to by the slice element.
+			// 	fmt.Printf("First Message Content: %s\n", messages[0].Content)
+
+			// 	// If you want to print the entire message struct, use %+v to print with field names for readability.
+			// 	fmt.Printf("First Message Struct: %+v\n", *messages[0])
+			// } else {
+			// 	fmt.Println("No messages found in the thread.")
+			// }
+
+			// Define thread creation parameter bs
 			threadCreateData := discordgo.MessageSend{
 				// Content: m.Content,
 				Content: "This thread was created to replace a deleted thread.",
 			}
+
+			// // Delete the thread
+			// _, err = s.ChannelDelete(channel.ID)
+			// if err != nil {
+			// 	log.Printf("Error deleting thread: %v\n", err)
+			// 	return
+			// }
 
 			// Create a new thread with the same name as the deleted thread
 			newThread, err := s.ForumThreadStartComplex(parentChannel.ID, &threadStart, &threadCreateData)
